44c8da0722a4d1989f72409c833aa2217ac2145e
LuanRodrigues15 <luan15008090@gmail.com>
2026-03-09 23:10:41 -0300

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
+import { HashRouter } from 'react-router-dom' 
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './hooks/use-theme.tsx'
@@ -8,9 +8,9 @@ import { ThemeProvider } from './hooks/use-theme.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )

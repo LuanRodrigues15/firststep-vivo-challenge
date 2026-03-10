4fe6dcdf8415346396c20b18d69e0ead089eadb7
LuanRodrigues15 <luan15008090@gmail.com>
2026-03-09 23:07:20 -0300

    pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// 1. Importe o BrowserRouter
 import { BrowserRouter } from 'react-router-dom' 
 import App from './App.tsx'
 import './index.css'
@@ -9,8 +8,7 @@ import { ThemeProvider } from './hooks/use-theme.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {/* 2. Envolva o App com o BrowserRouter */}
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </ThemeProvider>
